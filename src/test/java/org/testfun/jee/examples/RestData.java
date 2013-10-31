65d9803a25417751cb82d2f6c967f5c5308818f7
Michael Yakobi <michaelyaakoby@gmail.com>
2013-11-01 00:51:26 +0200

    extended jax-rs test to demonstrate usage of mocks

--- src/test/java/org/testfun/jee/examples/RestData.java
+++ src/test/java/org/testfun/jee/examples/RestData.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
