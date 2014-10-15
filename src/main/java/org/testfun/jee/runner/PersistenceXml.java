10f03191cd1ce1be384f35ca0b8d2a6aadbb127b
uaiHebert <hebolive@gmail.com>
2014-10-15 17:03:27 -0300

    adding suport for jpa or hibernate url

--- src/main/java/org/testfun/jee/runner/PersistenceXml.java
+++ src/main/java/org/testfun/jee/runner/PersistenceXml.java
@@ -1,6 +1,7 @@
 package org.testfun.jee.runner;
 
 import lombok.Data;
+import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -32,12 +33,36 @@ public class PersistenceXml {
         try {
             XPath xPath = XPathFactory.newInstance().newXPath();
 
-            connectionUrl = (String) xPath.evaluate("//*[local-name()='property' and @name='hibernate.connection.url']/@value", document, XPathConstants.STRING);
+            connectionUrl = extractConnectionURL(document, xPath);
             persistenceUnitName = (String) xPath.evaluate("//*[local-name()='persistence-unit']/@name", document, XPathConstants.STRING);
 
         } catch (XPathExpressionException e) {
             throw new EjbWithMockitoRunnerException("Failed initializing XPath expressions");
         }
+    }
+
+    private String extractConnectionURL(final Document document, final XPath xPath) throws XPathExpressionException {
+        String connectionURL = getHibernateURL(document, xPath);
+
+        if (StringUtils.isBlank(connectionURL)) {
+            connectionURL = getJPAURL(document, xPath);
+        }
+
+        if (StringUtils.isBlank(connectionURL)){
+            final String errorMessage = "We could not find the JPA/Hibernate jdbc URL. " +
+                    "\n Did you set the hibernate.connection.url or the javax.persistence.jdbc.url attribute in the persistence.xml file?";
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        return connectionURL;
+    }
+
+    private String getHibernateURL(final Document document, final XPath xPath) throws XPathExpressionException {
+        return (String) xPath.evaluate("//*[local-name()='property' and @name='hibernate.connection.url']/@value", document, XPathConstants.STRING);
+    }
+
+    private String getJPAURL(final Document document, final XPath xPath) throws XPathExpressionException {
+        return (String) xPath.evaluate("//*[local-name()='property' and @name='javax.persistence.jdbc.url']/@value", document, XPathConstants.STRING);
     }
 
     public static PersistenceXml getInstnace() {
